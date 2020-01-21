635616846e0262903775d55f087dcf286edd8811
luczakm1 <mateusz.luczak.S@ee.pw.edu.pl>
2020-01-21 13:07:10 +0100

    Init 2

--- src/main/java/pl/pw/ocd/app/AppApplication.java
+++ src/main/java/pl/pw/ocd/app/AppApplication.java
@@ -1,9 +1,13 @@
 package pl.pw.ocd.app;
 
+import lombok.extern.java.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@Log
+@SpringBootApplication(scanBasePackages = {
+    "pl.pw.ocd.app.configuration"
+})
 public class AppApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/pl/pw/ocd/app/configuration/RedisConfiguration.java
@@ -0,0 +1,27 @@
+package pl.pw.ocd.app.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfiguration {
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConf = new RedisStandaloneConfiguration();
+        redisConf.setHostName("localhost");
+
+        return new LettuceConnectionFactory(redisConf);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
+}
