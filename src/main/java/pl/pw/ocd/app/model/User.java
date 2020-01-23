1212af73b571348e508dc1d6672623d61deda7c0
WinCubeML <mati031398@gmail.com>
2020-01-23 21:25:19 +0100

    Part I: User model

---
+++ src/main/java/pl/pw/ocd/app/model/User.java
@@ -0,0 +1,24 @@
+package pl.pw.ocd.app.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@RedisHash("user")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    private String login;
+
+    private String password;
+
+    private String name;
+
+    private String surname;
+
+    private String pesel;
+}
