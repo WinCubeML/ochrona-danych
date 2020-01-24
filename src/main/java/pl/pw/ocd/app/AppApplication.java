c15b23c50d40d73046b3becae341623500d624ef
WinCubeML <mati031398@gmail.com>
2020-01-24 20:06:00 +0100

    Part I: Finishing initials for whole project

--- src/main/java/pl/pw/ocd/app/AppApplication.java
+++ src/main/java/pl/pw/ocd/app/AppApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Log
 @SpringBootApplication(scanBasePackages = {
-    "pl.pw.ocd.app.configuration"
+        "pl.pw.ocd.app.configuration", "pl.pw.ocd.app.controller", "pl.pw.ocd.app.exceptions", "pl.pw.ocd.app.logic", "pl.pw.ocd.app.model", "pl.pw.ocd.app.repositories", "pl.pw.ocd.app.service"
 })
 public class AppApplication {
 
