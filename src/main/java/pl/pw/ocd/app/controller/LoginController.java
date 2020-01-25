5106fde15dc45537590db12cd3950cceb42dc0b3
WinCubeML <mati031398@gmail.com>
2020-01-25 14:03:38 +0100

    Part II: Login page

--- src/main/java/pl/pw/ocd/app/controller/LoginController.java
+++ src/main/java/pl/pw/ocd/app/controller/LoginController.java
@@ -2,7 +2,11 @@ package pl.pw.ocd.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import pl.pw.ocd.app.logic.PasswordEncoder;
+import pl.pw.ocd.app.model.LoginDTO;
 import pl.pw.ocd.app.service.LoginService;
 import pl.pw.ocd.app.service.UserService;
 
@@ -16,4 +20,11 @@ public class LoginController {
 
     @Autowired
     PasswordEncoder passwordEncoder;
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView getLoginPage() {
+        ModelAndView modelAndView = new ModelAndView("login");
+        modelAndView.addObject("loginDTO", new LoginDTO());
+        return modelAndView;
+    }
 }
