d9ba9c3fb60f6b5d7b9e457345abc442db161488
WinCubeML <mati031398@gmail.com>
2020-01-25 18:10:57 +0100

    Part III: Note service init

--- src/main/java/pl/pw/ocd/app/service/NoteService.java
+++ src/main/java/pl/pw/ocd/app/service/NoteService.java
@@ -1,7 +1,23 @@
 package pl.pw.ocd.app.service;
 
 import org.springframework.stereotype.Service;
+import pl.pw.ocd.app.model.Note;
+
+import java.util.List;
 
 @Service
 public interface NoteService {
+    void createNote(Note note);
+
+    Note getNote(String noteId);
+
+    List<Note> getPermittedNotes(String login);
+
+    List<Note> getAllNotes();
+
+    Note modifyNote(Note oldNote, Note newNote);
+
+    Note modifyPermitted(Note note, String logins);
+
+    void deleteNote(String noteId);
 }
--- src/main/java/pl/pw/ocd/app/service/NoteServiceImpl.java
+++ src/main/java/pl/pw/ocd/app/service/NoteServiceImpl.java
@@ -1,7 +1,44 @@
 package pl.pw.ocd.app.service;
 
 import org.springframework.stereotype.Service;
+import pl.pw.ocd.app.model.Note;
+
+import java.util.List;
 
 @Service
 public class NoteServiceImpl implements NoteService {
+    @Override
+    public void createNote(Note note) {
+
+    }
+
+    @Override
+    public Note getNote(String noteId) {
+        return null;
+    }
+
+    @Override
+    public List<Note> getPermittedNotes(String login) {
+        return null;
+    }
+
+    @Override
+    public List<Note> getAllNotes() {
+        return null;
+    }
+
+    @Override
+    public Note modifyNote(Note oldNote, Note newNote) {
+        return null;
+    }
+
+    @Override
+    public Note modifyPermitted(Note note, String logins) {
+        return null;
+    }
+
+    @Override
+    public void deleteNote(String noteId) {
+
+    }
 }
